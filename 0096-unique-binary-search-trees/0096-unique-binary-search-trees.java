ef5bd38611708ad0a5c18c1d55eb75d90f59d5bc
Shrikant Mahore <shrikantmahore981@gmail.com>
2025-06-08 00:50:44 +0530

    Time: 2295 ms (8.91%), Space: 40.9 MB (6.4%) - LeetHub

--- 0096-unique-binary-search-trees/0096-unique-binary-search-trees.java
+++ 0096-unique-binary-search-trees/0096-unique-binary-search-trees.java
@@ -1,15 +1,14 @@
 class Solution {
     public int numTrees(int n) {
-        int[] dp = new int[n + 1];
-        dp[0] = 1; // Empty tree
-        dp[1] = 1; // One node
+        return fxn(n);
+    }
+    static int fxn(int n ){
 
-        for (int i = 2; i <= n; i++) {
-            for (int j = 1; j <= i; j++) {
-                dp[i] += dp[j - 1] * dp[i - j];
-            }
+        if (n <= 1) return 1;
+        int total=0;
+         for (int i = 0; i < n; i++) {
+            total += fxn(i) * fxn(n - 1 - i); // left * right
         }
-
-        return dp[n];
+        return total;
     }
 }
