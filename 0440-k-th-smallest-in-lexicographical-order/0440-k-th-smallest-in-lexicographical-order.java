9ff7aa338d62ba15eaffc3b6ecf8fbe38c66a73c
Shrikant Mahore <shrikantmahore981@gmail.com>
2025-06-09 20:23:18 +0530

    Time: 0 ms (100%), Space: 40.4 MB (50.23%) - LeetHub

---
+++ 0440-k-th-smallest-in-lexicographical-order/0440-k-th-smallest-in-lexicographical-order.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int findKthNumber(int n, int k) {
+        int curr = 1;
+        k -= 1;
+        
+        while (k > 0) {
+            long steps = countSteps(n, curr, curr + 1);
+            if (steps <= k) {
+                k -= steps;
+                curr += 1;
+            } else {
+                curr *= 10;
+                k -= 1;
+            }
+        }
+        
+        return curr;
+    }
+
+    private long countSteps(int n, long curr, long next) {
+        long steps = 0;
+        while (curr <= n) {
+            steps += Math.min(n + 1L, next) - curr;
+            curr *= 10;
+            next *= 10;
+        }
+        return steps;
+    }
+}
